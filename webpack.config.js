a611d95a4e44b24189650e476a5a0e635f323fe8
OlhaKlymas <olhaklymas@gmail.com>
2018-03-11 16:50:33 +0200

    f3

--- webpack.config.js
+++ webpack.config.js
@@ -44,3 +44,20 @@ module:{
 	]
 }
 };
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
