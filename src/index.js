ccb00eba8942b36dc4e9387d61ef831838451c30
OlhaKlymas <olhaklymas@gmail.com>
2018-03-14 13:05:39 +0200

    b1

--- src/index.js
+++ src/index.js
@@ -35,28 +35,31 @@ $('.button-group').each(function(i, buttonGroup) {
   });
 });
   
-
-
 function loadCurrencies(){
   let from = document.getElementById('from');
   let to = document.getElementById('to');
   let xHttp = new XMLHttpRequest();
   xHttp.onreadystatechange = function(){
-    if(xHttp.readyState == 4 && xHttp.status == 200){
+    if(xHttp.readyState==4 && xHttp.status==200){
       let obj = JSON.parse(this.responseText);
       let options = '';
       for(key in obj.rates){
         options = options + '<option>'+key+'</option>';
       }
-      from.innerHtml = options;
-      to.innerHtml = options;
+      from.innerHTML = options;
+      to.innerHTML = options;
     }
   }
-  xHttp.open('GET','https://api.fixer.io/latest', true);
+  xHttp.open('GET','http://api.fixer.io/latest', true);
   xHttp.send();
 }
 
-function converterCurrency(){
+window.onload = loadCurrencies();
+document.getElementById("amount").oninput = convertCurrency();
+document.getElementsByTagName("select").onchange = convertCurrency();
+
+
+function convertCurrency(){
   let from = document.getElementById('from').value;
   let to = document.getElementById('to').value;
   let amount = document.getElementById('amount').value;
@@ -64,20 +67,18 @@ function converterCurrency(){
   if(from.length>0 && to.length>0 && amount.length>0){
     let xHttp = new XMLHttpRequest();
     xHttp.onreadystatechange = function(){
-      if(xHttp.readyState == 4 && xHttp.status == 200){
+      if(xHttp.readyState==4 && xHttp.status==200){
         let obj = JSON.parse(this.responseText);
         let fact = obj.rates[to];
         if(fact!=undefined){
-          result.innerHtml = parseFloat(amount)*parseFloat(fact);
+          result.innerHTML = parseFloat(amount)*parseFloat(fact);
         }
       }
     }    
-  xHttp.open('GET','https://api.fixer.io/latest'+from+'&symbols'+to, true);
+  xHttp.open('GET','http://api.fixer.io/latest?base='+from+'&symbols='+to, true);
   xHttp.send();
   }
 }
-
-
 
 var radialCanvas = document.getElementById("radialChart");
 
